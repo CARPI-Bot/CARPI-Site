f41ddd7a0d2f0f044bcedc1d9bbbe0337f8eca4a
Lawrence M <lawrencejunzh@gmail.com>
2024-09-24 17:09:16 -0400

    add react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Planner from './components/Planner';
+import About from './components/About';
+import Settings from './components/Settings';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">About</Link></li>
+          <li><Link to="/planner">Planner</Link></li>
+          <li><Link to="/settings">Settings</Link></li>
+
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/planner" element={<Planner />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Planner.js
@@ -0,0 +1,5 @@
+const Planner = () => {
+    return <h2>Planner Page</h2>;
+};
+
+export default Planner;
