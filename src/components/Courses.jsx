d52bbc20512041a4646a892b5035cf8ce94d122a
Gavin Liu <85533492+liug88@users.noreply.github.com>
2024-10-11 16:55:31 -0400

    Course Entires (with props)
    The course search function now works with props, by adding courses in a now json-friendly format. Added a test mock database of 2000 entries (about how much RPI courses has)

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,39 +1,56 @@
 import { useState } from 'react';
 
-const Courses = () => {
-  const [isOpen, setIsOpen] = useState(false);
+const Courses = ({ courses }) => {
+  const [openCourses, setOpenCourses] = useState(
+    Array(courses.length).fill(false),
+  );
 
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
+  const toggleDropdown = (index) => {
+    setOpenCourses((prev) => {
+      const newOpenCourses = [...prev];
+      newOpenCourses[index] = !newOpenCourses[index];
+      return newOpenCourses;
+    });
   };
 
   return (
-    <div className="bg-gray-100 bg-contain p-2 mt-4 rounded-xl" style={styles.Container}>
-      <div className="Course" style={styles.Course} onClick={toggleDropdown}>
-        <div className="Names" style={styles.Names}>
-          <h1 className="CourseID" style={styles.CourseID}>
-            CourseID
-          </h1>
-          <h1 className="CourseName" style={styles.CourseName}>
-            CourseName
-          </h1>
+    <div
+      className="bg-gray-100 bg-contain p-2 mt-4 rounded-xl"
+      style={styles.Container}
+    >
+      {courses.map((course, index) => (
+        <div key={course.id}>
+          <div
+            className="Course"
+            style={styles.Course}
+            onClick={() => toggleDropdown(index)}
+          >
+            <div className="Names" style={styles.Names}>
+              <h1 className="CourseID" style={styles.CourseID}>
+                {course.id}
+              </h1>
+              <h1 className="CourseName" style={styles.CourseName}>
+                {course.name}
+              </h1>
+            </div>
+            <h1 className="CourseCredit" style={styles.CourseCredit}>
+              {course.credits} Credits
+            </h1>
+          </div>
+          {openCourses[index] && (
+            <div className="CourseBlurb" style={styles.CourseBlurb}>
+              <p>{course.description}</p>
+            </div>
+          )}
         </div>
-        <h1 className="CourseCredit" style={styles.CourseCredit}>
-          4 Credits
-        </h1>
-      </div>
-      {isOpen && (
-        <div className="CourseBlurb" style={styles.CourseBlurb}>
-          <p>This is a brief description of the course. It provides an overview of what the course is about and what students can expect to learn.</p>
-        </div>
-      )}
+      ))}
     </div>
   );
 };
 
 const styles = {
   Container: {
-    maxHeight: '400px', // Adjust the height as needed
+    maxHeight: '400px',
     overflowY: 'scroll',
   },
   Course: {
