5a28523e469b5bbe4f8fa034c2051ed2967c52c9
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2024-10-25 17:16:36 -0400

    added a separate style section
    added a separate style section instead of including it inside the markup.

--- src/components/SearchCourse.jsx
+++ src/components/SearchCourse.jsx
@@ -1,24 +1,55 @@
 import Courses from '../components/Courses';
+
 const courseData = Array.from({ length: 2000 }, (_, index) => ({
   id: `Test ${index + 1}`,
   name: `CourseName ${index + 1}`,
   credits: 4,
   description: `This is a brief description of Course ${index + 1}.`,
 }));
+
 const SearchCourse = () => {
   return (
-    <div className="bg-red-100 bg-contain w-6/12 p-7 ml-5 rounded-xl">
-      <h1 className="text-left text-4xl  ">Search Course</h1>
-      <input
-        type="text"
-        className="drop-shadow-md w-full p-3 rounded-lg "
-        placeholder="Find Courses Here..."
-      />
+    <div style={styles.container}>
+      <h1 style={styles.heading}>Search Course</h1>
+      <div style={styles.searchAndFilter}>
+        <input
+          type="text"
+          style={styles.input}
+          placeholder="Find Courses Here..."
+        />
+        <div  type="text">
+                    Filter
+        </div>
+      </div>
       <div>
         <Courses courses={courseData} />
       </div>
     </div>
   );
+};
+
+const styles = {
+  container: {
+    backgroundColor: 'rgb(254, 226, 226)',
+    width: '50%',
+    padding: '1.75rem',
+    marginLeft: '1.25rem',
+    borderRadius: '0.75rem',
+  },
+  heading: {
+    textAlign: 'left',
+    fontSize: '2.25rem',
+    margin: '0px',
+  },
+  input: {
+    boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)', 
+    width: '100%',
+    padding: '0.75rem',
+    borderRadius: '0.5rem',
+  },
+  searchAndFilter: {
+    flexDirection: 'row',
+  },
 };
 
 export default SearchCourse;
