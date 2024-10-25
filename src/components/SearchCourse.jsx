d3a7afe6a02c045e8781ae1b04cec12947edb710
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2024-10-25 17:45:12 -0400

    filter bar + css
    added the filter bar which will appear once the Filter button is pressed.
    
    when the button is pressed, showFilterOptions will toggle the button on and off.

--- src/components/SearchCourse.jsx
+++ src/components/SearchCourse.jsx
@@ -1,32 +1,5 @@
+import { useState } from 'react';
 import Courses from '../components/Courses';
-
-const courseData = Array.from({ length: 2000 }, (_, index) => ({
-  id: `Test ${index + 1}`,
-  name: `CourseName ${index + 1}`,
-  credits: 4,
-  description: `This is a brief description of Course ${index + 1}.`,
-}));
-
-const SearchCourse = () => {
-  return (
-    <div style={styles.container}>
-      <h1 style={styles.heading}>Search Course</h1>
-      <div style={styles.searchAndFilter}>
-        <input
-          type="text"
-          style={styles.input}
-          placeholder="Find Courses Here..."
-        />
-        <div  type="text">
-                    Filter
-        </div>
-      </div>
-      <div>
-        <Courses courses={courseData} />
-      </div>
-    </div>
-  );
-};
 
 const styles = {
   container: {
@@ -39,17 +12,113 @@ const styles = {
   heading: {
     textAlign: 'left',
     fontSize: '2.25rem',
-    margin: '0px',
+  },
+  searchFilterContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: '10px',
+    marginBottom: '10px',
   },
   input: {
-    boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)', 
+    boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
     width: '100%',
     padding: '0.75rem',
     borderRadius: '0.5rem',
+    backgroundColor: '#f3f4f6',
   },
-  searchAndFilter: {
+  filter: {
+    padding: '0.75rem',
+    borderRadius: '0.5rem',
+    backgroundColor: 'rgb(254, 226, 226)',
+    width: '30%',
+  },
+  filterAttribute: {
+    fontSize: '15px',
+    backgroundColor: '#f3f4f6',
+  },
+  filterOptions: {
+    display: 'flex',
     flexDirection: 'row',
+    gap: '1rem',
+    padding: '0.75rem',
+
+    backgroundColor: '#f3f4f6',
+    boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+    borderRadius: '0.5rem',
+    
+    marginTop: '10px',
   },
+  attribute: {
+    fontSize: '10px',
+    color: '#707070',
+  },
+};
+
+const courseData = Array.from({ length: 2000 }, (_, index) => ({
+  id: `Test ${index + 1}`,
+  name: `CourseName ${index + 1}`,
+  credits: 4,
+  description: `This is a brief description of Course ${index + 1}.`,
+}));
+
+const SearchCourse = () => {
+  const [showFilterOptions, setShowFilterOptions] = useState(false);
+
+  return (
+    <div style={styles.container}>
+      <h1 style={styles.heading}>Search Course</h1>
+      <div style={styles.searchFilterContainer}>
+        <input
+          type="text"
+          style={styles.input}
+          placeholder="Find Courses Here..."
+        />
+        <div
+          style={styles.filter}
+          onClick={() => setShowFilterOptions(!showFilterOptions)}
+        >
+          Filter
+        </div>
+      </div>
+      {showFilterOptions && (
+        <div style={styles.filterOptions}>
+          <div> 
+            <div style={styles.attribute}>Subject</div>
+            <select style={styles.filterAttribute} >
+              <option value="">All Subjects</option>
+              <option value="programming">rahh</option>
+              <option value="math">rahh2</option>
+              <option value="science">rahh3</option>
+            </select>
+          </div>
+
+          <div> 
+            <div style={styles.attribute}>Attribute</div>
+            <select style={styles.filterAttribute} >
+              <option value="">All Attributes</option>
+              <option value="programming">Programming</option>
+              <option value="math">Math</option>
+              <option value="science">Science</option>
+            </select>
+          </div>
+
+          <div> 
+            <div style={styles.attribute}>Semesters Offered</div>
+            <select style={styles.filterAttribute} >
+              <option value="">All Semesters</option>
+              <option value="programming">Programming</option>
+              <option value="math">Math</option>
+              <option value="science">Science</option>
+            </select>
+          </div>
+        </div>
+      )}
+      <div>
+        <Courses courses={courseData} />
+      </div>
+    </div>
+  );
 };
 
 export default SearchCourse;
