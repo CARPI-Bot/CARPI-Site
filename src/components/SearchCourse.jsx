dcd1e6f3e4320b8c1a51ee79e580c174bbc92d4e
Gavin Liu <85533492+liug88@users.noreply.github.com>
2024-11-01 16:52:28 -0400

    React DND + Code formatting
    Removed errant spaces, and also installed React DND for later testing

--- src/components/SearchCourse.jsx
+++ src/components/SearchCourse.jsx
@@ -1,17 +1,19 @@
 import Courses from '../components/Courses';
+
 const courseData = Array.from({ length: 2000 }, (_, index) => ({
   id: `Test ${index + 1}`,
   name: `CourseName ${index + 1}`,
   credits: 4,
   description: `This is a brief description of Course ${index + 1}.`,
 }));
+
 const SearchCourse = () => {
   return (
     <div className="bg-red-100 bg-contain w-6/12 p-7 ml-5 rounded-xl">
-      <h1 className="text-left text-4xl  ">Search Course</h1>
+      <h1 className="text-left text-4xl">Search Course</h1>
       <input
         type="text"
-        className="drop-shadow-md w-full p-3 rounded-lg "
+        className="drop-shadow-md w-full p-3 rounded-lg"
         placeholder="Find Courses Here..."
       />
       <div>
