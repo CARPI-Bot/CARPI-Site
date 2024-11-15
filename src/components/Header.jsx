93bd42220938e9333957d98b3e293e906e6d5d70
William Chen <williamchen259@gmail.com>
2024-11-15 16:19:13 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
 import CARPI from '../assets/images/Transparent_Carpi.svg';
 import '../assets/css/font.css';
 
+import { useState } from 'react';
+
 function Header() {
+  const [clickCount, setClickCount] = useState(0);
+
+  const handleClick = () => {
+    setClickCount((prevCount) => {
+      if (prevCount + 1 === 10) {
+        alert('Fish ');
+        return 0;
+      }
+      return prevCount + 1;
+    });
+  };
+
   return (
     <header className="bg-black-600 text-black mt-4 mb-2 flex justify-between">
       <div className="container mx-auto flex flex-row justify-between items-center">
@@ -9,6 +23,7 @@ function Header() {
           className="size-24 rounded-full ml-8"
           src={CARPI}
           alt="Filler"
+          onClick={handleClick}
         ></img>
         <h1 className="text-3xl font-bold" style={styles.CARPI}>
           CARPI
